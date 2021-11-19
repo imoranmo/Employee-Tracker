d44df2e5250b4cff0854090d43826020f87304ea
imoranmo <imoranmo@gmail.com>
2021-11-19 08:48:56 -0800

    adding git congig

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  port: 3301,
+  port: 3306,
   user: 'root',
   password: process.env.DB_PASSWORD,
   database: 'employee_db'
